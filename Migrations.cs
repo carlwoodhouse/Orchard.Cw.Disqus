4198458ea0034af3714115edffe3f06975d6fffc
Carl Woodhouse <carl.woodhouse@gmail.com>
2013-11-08 21:39:24 +0000

    first commit

---
+++ Migrations.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Orchard.Cw.Disqus
+{
+    using Orchard.ContentManagement.MetaData;
+    using Orchard.Core.Contents.Extensions;
+    using Orchard.Data.Migration;
+
+    using Orchard.Cw.Disqus.Models;
+
+    /// <summary>migration for breadcrumbs</summary>
+    public class Migrations : DataMigrationImpl
+    {
+        public int Create()
+        {
+            ContentDefinitionManager.AlterPartDefinition(
+                typeof(DisqusPart).Name, cfg => cfg.Attachable());
+
+            return 1;
+        }
+    }
+}
---
+++ Models/DisqusPart.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Orchard.Cw.Disqus.Models
+{
+    using Orchard.ContentManagement;
+
+    /// <summary>part for disquss comments</summary>
+    public class DisqusPart : ContentPart
+    {
+    }
+}
