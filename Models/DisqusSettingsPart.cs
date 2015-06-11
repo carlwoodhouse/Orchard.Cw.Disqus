43dc68c1a6ff1994bd08c5c38fb95d3d7e48aaf4
Carl Woodhouse <carl.woodhouse@gmail.com>
2015-06-11 16:31:33 +0100

    fixes

--- Models/DisqusSettingsPart.cs
+++ Models/DisqusSettingsPart.cs
@@ -2,15 +2,20 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Orchard.Cw.Disqus.Models
 {
-    public class DisqusSettingsPart : ContentPart
+    public class DisqusSettingsPart : ContentPart<DisqusSettingsPart>
     {
-        [DisplayName("Your disqus shortname")]
-        public string Shortname { get; set; }
+        [Required, DisplayName("Your disqus shortname")]
+        public string Shortname 
+        {
+            get { return this.Retrieve(x => x.Shortname); }
+            set { this.Store(x => x.Shortname, value); }
+        }
     }
 }
