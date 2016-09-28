3e4144268050551fc8bb45f7ee5b854466f393da
Carl Woodhouse <carl.woodhouse@gmail.com>
2016-09-28 21:32:18 +0100

    Added the ability to hide comments on a per contentitem basis

--- Drivers/DisqusPartDriver.cs
+++ Drivers/DisqusPartDriver.cs
@@ -14,9 +14,27 @@ namespace Orchard.Cw.Disqus.Drivers {
 
         protected override DriverResult Display(DisqusPart part, string displayType, dynamic shapeHelper)
         {
+            if (part.Hide) {
+                return null;
+            }
+
             return ContentShape("Parts_Disqus", () => {
                 return shapeHelper.Parts_Disqus(Shortname : _orchardServices.WorkContext.CurrentSite.As<DisqusSettingsPart>().Shortname);
             });
+        }
+
+        protected override DriverResult Editor(DisqusPart part, dynamic shapeHelper) {
+            return ContentShape("Parts_Disqus_Edit",
+                () => shapeHelper.EditorTemplate(TemplateName: "Parts.Disqus.Edit", Model: part, Prefix: Prefix));
+        }
+
+        protected override DriverResult Editor(DisqusPart part, IUpdateModel updater, dynamic shapeHelper) {
+            if (updater != null) {
+                updater.TryUpdateModel(part, Prefix, null, null);
+            }
+
+            return ContentShape("Parts_Disqus_Edit",
+               () => shapeHelper.EditorTemplate(TemplateName: "Parts.Disqus.Edit", Model: part, Prefix: Prefix));
         }
     }
 }
--- Models/DisqusPart.cs
+++ Models/DisqusPart.cs
@@ -1,5 +1,17 @@
 ﻿using Orchard.ContentManagement;
 
 namespace Orchard.Cw.Disqus.Models {
-    public class DisqusPart : ContentPart { }
+    public class DisqusPart : ContentPart {
+        public bool Hide
+        {
+            get
+            {
+                return this.Retrieve(x => Hide);
+            }
+            set
+            {
+                this.Store(x => Hide, value);
+            }
+        }
+    }
 }
